0c6e9a307316df8091d8f94d8bb56d0deb7b7c9f
Julien <julien@mezimail.com>
2013-09-29 14:00:00 +0200

    fdf

---
+++ app.js
@@ -0,0 +1,61 @@
+﻿/// <reference path="~/lib/node-vsdoc.js" />
+
+/**
+ * Module dependencies.
+ */
+
+var config = require('./config.json');
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    main = require('./main'),
+    PORT = process.env.PORT || config.env.PORT,
+    app = express();
+
+app.use(express.logger("dev"));
+
+app.use(express.bodyParser({
+    uploadDir: __dirname + '/uploads',
+    keepExtensions: true
+}));
+
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, './uploads')));
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+ 
+// Mise à jour via POST
+//app.post('/data-users/:id', routes.users.update);
+ 
+// Ajout via POST
+app.get('/test', function(req, res){
+    console.log("test");
+    res.write("ok");
+    res.end();
+});
+app.get('/send-cahier/:id', main.addImage);
+app.post('/send-cahier/:id', main.addImage);
+app.post('/send-photo-cahier/:id', main.addImage);
+
+
+
+app.post('/images', main.addImage); // endpoint to post new images
+app.get('/images', main.getImages); // endpoint to get list of images
+
+if (!module.parent) {
+    app.listen(PORT);
+    console.log('Server running at http://127.0.0.1:' + PORT);
+}
+
+process.on('uncaughtException', function (exception) {
+    //exception.response.writeHead(exception.code, {'Content-Type': 'text/html'});
+    //exception.response.end('Error ' + exception.code + ' - ' + exception.message);
+});
