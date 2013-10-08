69c378d29eea3631cc041575a42737470647e95a
Julien <julien@mezimail.com>
2013-10-08 23:43:32 +0200

    gr

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.bodyParser({
 }));
 
 app.use(express.methodOverride());
-//app.use(app.router);
+app.use(app.router);
 app.use(express.static(path.join(__dirname, './public')));
 
 app.configure('development', function(){
